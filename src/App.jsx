55d51310373e7015330bb610cc596c535dd247a3
conradogui <gui14fbce@gmail.com>
2024-05-02 16:18:10 -0300

    feat/alterando componente de pop up gato

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,11 @@
+import CatCard from "./components/CatCard"
 import './index.css'
-import { Button } from "@/components/ui/button";
-
 
 function App() {
 
   return (
     <>
-     <h1>hello world</h1>
-     <Button variant="outline">Shadcn UI Button</Button>
-
+     <CatCard/>
     </>
   )
 }
