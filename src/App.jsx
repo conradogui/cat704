a2494f18fee61e82d164eae8ce6e3cfa5470fabf
conradogui <gui14fbce@gmail.com>
2024-05-02 14:28:32 -0300

    inicio projeto

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './index.css'
+import { Button } from "@/components/ui/button";
+
+
+function App() {
+
+  return (
+    <>
+     <h1>hello world</h1>
+     <Button variant="outline">Shadcn UI Button</Button>
+
+    </>
+  )
+}
+
+export default App
